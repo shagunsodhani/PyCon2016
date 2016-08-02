9b80a160af178bfe8c219888c9fe0d7dab2b3873
shagun Sodhani <sshagunsodhani@gmail.com>
2016-08-02 21:36:12 +0530

    Replaced AskUbuntu Analysis notebook by StackOverflow Analysis notebook

--- notebook/run.sh
+++ notebook/run.sh
@@ -1,1 +1,1 @@
-PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook" $SPARK_HOME/bin/pyspark --master local[3] --driver-memory 2g --executor-memory 2g
+PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook" $SPARK_HOME/bin/pyspark --master local[2]
